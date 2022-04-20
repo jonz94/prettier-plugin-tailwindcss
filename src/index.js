bcdd851af56e3f3037be5035ae786c421f2f7e9f
jonz94 <jody16888@gmail.com>
2022-04-20 16:23:15 +0800

    Apply patch

--- src/index.js
+++ src/index.js
@@ -115,7 +115,7 @@ function sortClasses(classStr, { env, ignoreFirst = false, ignoreLast = false })
   return prefix + result + suffix
 }
 
-function createParser(original, transform) {
+export function createParser(original, transform) {
   return {
     ...original,
     parse(text, parsers, options = {}) {
@@ -182,7 +182,7 @@ function createParser(original, transform) {
   }
 }
 
-function transformHtml(attributes, computedAttributes = []) {
+export function transformHtml(attributes, computedAttributes = []) {
   let transform = (ast, { env }) => {
     for (let attr of ast.attrs ?? []) {
       if (attributes.includes(attr.name)) {
@@ -291,7 +291,7 @@ function sortTemplateLiteral(node, { env }) {
   return didChange
 }
 
-function transformJavaScript(ast, { env }) {
+export function transformJavaScript(ast, { env }) {
   visit(ast, {
     JSXAttribute(node) {
       if (!node.value) {
@@ -314,7 +314,7 @@ function transformJavaScript(ast, { env }) {
   })
 }
 
-function transformCss(ast, { env }) {
+export function transformCss(ast, { env }) {
   ast.walk((node) => {
     if (node.type === 'css-atrule' && node.name === 'apply') {
       node.params = sortClasses(node.params, {
@@ -388,7 +388,7 @@ export const parsers = {
   }),
 }
 
-function transformSvelte(ast, { env, changes }) {
+export function transformSvelte(ast, { env, changes }) {
   for (let attr of ast.attributes ?? []) {
     if (attr.name === 'class' && attr.type === 'Attribute') {
       for (let i = 0; i < attr.value.length; i++) {
